bc0e657c077045ed826724e6f3138412028acf40
teccnik <57708724+teccnik@users.noreply.github.com>
2020-12-09 19:24:21 +0000

    Song upload working, in addition to searching for tracks. Just need to test that the audio encoding & embed is working, so people can listen to songs. Search results showing well to index.html page - might change this to a jquery to parse a searchString over to results.html (but just need to figure out how to do that, after window.open("results.html?searchItem=searchString","_self")

--- resources/client/scripts/searchFunction.js
+++ resources/client/scripts/searchFunction.js
@@ -1,8 +1,8 @@
-function getSearch() {
+/*function getSearch() {
     console.log("getSearch() Invoked.");
     let valid = true;
     document.getElementById("search-validationtext").innerHTML='';
     if (document.getElementById('search-item').value="") {
         document.getElementById("search-validationtext").innerHTML="Please enter your search item.";
     }
-}
+}*/
